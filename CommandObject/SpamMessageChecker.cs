ef8cf566adcfb81c6722621fa8077a1a2c1e6e0f
OwO <OwO@OmO.tw>
2018-01-18 19:38:18 +0800

    change indexof to contains

--- CommandObject/SpamMessageChecker.cs
+++ CommandObject/SpamMessageChecker.cs
@@ -81,7 +81,7 @@ namespace CNBlackListSoamChecker.CommandObject
             int totalPoints = 0;
             foreach (SpamMessageObj msg in spamMessages)
             {
-                if (text.IndexOf(msg.Message) != -1)
+                if (text.Contains(msg.Message))
                 {
                     totalPoints += msg.Point;
                 }
