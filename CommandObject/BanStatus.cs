0ce36e719f8e5b8b6c4e00c651587f09e9a0414a
jorden2895 <jorden2895@gmail.com>
2018-01-25 07:01:06 +0800

    字串修正

--- CommandObject/BanStatus.cs
+++ CommandObject/BanStatus.cs
@@ -14,7 +14,7 @@
                         string banmsg = "";
                         BanUser ban;
                         ban = Temp.GetDatabaseManager().GetUserBanStatus(RawMessage.GetSendUser().id);
-                        banmsg = "發送者 : \n" + RawMessage.GetSendUser().GetUserTextInfo() + "\n" + ban.GetBanMessage();
+                        banmsg = "發送者 : " + RawMessage.GetSendUser().GetUserTextInfo() + "\n" + ban.GetBanMessage();
                         if (ban.Ban == 0)
                         {
                             banmsg += "\n對於被封鎖的使用者，你可以通過 [點選這裡](https://t.me/" + TgApi.getDefaultApiConnection().getMe().username + "?start=soam_req_unban) 以請求解除。";
@@ -22,13 +22,13 @@
                         if (RawMessage.reply_to_message != null)
                         {
                             ban = Temp.GetDatabaseManager().GetUserBanStatus(RawMessage.reply_to_message.GetSendUser().id);
-                            banmsg += "\n被回覆的訊息的原發送使用者 : \n" +
+                            banmsg += "\n\n被回覆的訊息的原發送使用者 : " +
                                 RawMessage.reply_to_message.GetSendUser().GetUserTextInfo() + "\n" +
                                 ban.GetBanMessage();
                             if (RawMessage.reply_to_message.forward_from != null)
                             {
                                 ban = Temp.GetDatabaseManager().GetUserBanStatus(RawMessage.reply_to_message.forward_from.id);
-                                banmsg += "\n被回覆的訊息轉發自使用者 : \n" +
+                                banmsg += "\n\n被回覆的訊息轉發自使用者 : " +
                                     RawMessage.reply_to_message.forward_from.GetUserTextInfo() + "\n" +
                                     ban.GetBanMessage();
                             }
