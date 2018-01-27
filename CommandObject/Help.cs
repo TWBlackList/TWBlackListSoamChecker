924b0aaedddf74b3e145e6dd70d027709e0adb70
OwO <OwO@OmO.tw>
2018-01-27 23:38:57 +0800

    :fix help

--- CommandObject/Help.cs
+++ CommandObject/Help.cs
@@ -31,8 +31,11 @@ namespace TWBlackListSoamChecker.CommandObject {
                 finalHelpMsg = finalHelpMsg + "\n\nOperator指令:\n" +
                                 "/twban - 封鎖\n" +
                                 "/twunban - 解除封鎖\n" +
-                                "/getspampoints - 測試關鍵字\n\n" +
-                                "Admin指令:\n" +
+                                "/getspampoints - 測試關鍵字\n\n";
+            }
+            if (RAPI.getIsBotOP(RawMessage.from.id))
+            {
+                finalHelpMsg = finalHelpMsg + "Admin指令:\n" +
                                 "/ban - 封鎖\n" +
                                 "/_ban - 批次封鎖\n" +
                                 "/unban - 解除封鎖\n" +
