04be8056a8c802ba4ffa44340b44949e0acf7514
OwO <OwO@OmO.tw>
2018-01-27 11:44:31 +0800

    fix typo

--- CommandObject/LeaveCommand.cs
+++ CommandObject/LeaveCommand.cs
@@ -4,9 +4,9 @@ using ReimuAPI.ReimuBase.TgData;
 namespace TWBlackListSoamChecker.CommandObject {
     internal class LeaveCommand {
         internal bool Leave(TgMessage RawMessage){
-            if (RAPI.getIsBotOP(RawMessage.GetSendUser().id) ||  RAPI.getIsBotAdmin(RawMessage.GetSendUser().id) || TgApi.getDefaultApiConnection().checkIsAdmin(message.chat.id, RawMessage.GetSendUser().id)){
+            if (RAPI.getIsBotOP(RawMessage.GetSendUser().id) ||  RAPI.getIsBotAdmin(RawMessage.GetSendUser().id) || TgApi.getDefaultApiConnection().checkIsAdmin(RawMessage.chat.id, RawMessage.GetSendUser().id)){
                 TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id, "由 使用者 (" + RawMessage.GetSendUser().id.ToString() + ") 請求離開群組" ,RawMessage.message_id);
-                TgApi.getDefaultApiConnection().leaveChat(message.chat.id);
+                TgApi.getDefaultApiConnection().leaveChat(RawMessage.chat.id);
                 return true;
             }else{
                 TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id, "您並不是群組管理或是 Bot 管理員" ,RawMessage.message_id);
