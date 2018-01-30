bd9ba50348092d101ff0000f3688e9f31137097a
OwO <OwO@OmO.tw>
2018-01-30 23:15:02 +0800

    fix user command

--- CommandObject/UserCommand.cs
+++ CommandObject/UserCommand.cs
@@ -4,6 +4,16 @@ using ReimuAPI.ReimuBase.TgData;
 namespace TWBlackListSoamChecker.CommandObject {
     internal class UserCommand {
         internal bool User(TgMessage RawMessage){
+            if (RawMessage.reply_to_message != null)
+                {
+                    if (RawMessage.reply_to_message.forward_from != null)
+                    {
+                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.forward_from.id,RawMessage.message_id);
+                        return true;
+                    }
+                    TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.GetSendUser().id,RawMessage.message_id);
+                    return true;
+                }
             TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.GetSendUser().id.ToString(),RawMessage.message_id);
             return true;
         }
