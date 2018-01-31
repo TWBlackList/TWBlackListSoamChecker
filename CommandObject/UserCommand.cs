b2a4f2c7831b3de574033c3ad20f5318ed407288
OwO <OwO@OmO.tw>
2018-01-31 10:42:44 +0800

    add new feature <get username> in user command

--- CommandObject/UserCommand.cs
+++ CommandObject/UserCommand.cs
@@ -4,18 +4,33 @@ using ReimuAPI.ReimuBase.TgData;
 namespace TWBlackListSoamChecker.CommandObject {
     internal class UserCommand {
         internal bool User(TgMessage RawMessage){
-            if (RawMessage.reply_to_message != null)
-                {
-                    if (RawMessage.reply_to_message.forward_from != null)
+            if(RawMessage.text.ToLower().Contains("username")){
+                if (RawMessage.reply_to_message != null)
                     {
-                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.forward_from.id.ToString(),RawMessage.message_id);
+                        if (RawMessage.reply_to_message.forward_from != null)
+                        {
+                            TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.forward_from.username.ToString(),RawMessage.message_id);
+                            return true;
+                        }
+                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.GetSendUser().username.ToString(),RawMessage.message_id);
                         return true;
                     }
-                    TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.GetSendUser().id.ToString(),RawMessage.message_id);
-                    return true;
-                }
-            TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.GetSendUser().id.ToString(),RawMessage.message_id);
-            return true;
+                TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.GetSendUser().username.ToString(),RawMessage.message_id);
+                return true;
+            }else{
+                if (RawMessage.reply_to_message != null)
+                    {
+                        if (RawMessage.reply_to_message.forward_from != null)
+                        {
+                            TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.forward_from.id.ToString(),RawMessage.message_id);
+                            return true;
+                        }
+                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.reply_to_message.GetSendUser().id.ToString(),RawMessage.message_id);
+                        return true;
+                    }
+                TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.GetSendUser().id.ToString(),RawMessage.message_id);
+                return true;
+            }
         }
     }
 }
