251ad9350c9facac42905309937e0ec93e6f81b6
OwO <OwO@OmO.tw>
2018-03-04 11:53:03 +0800

    [Point] remove text parm

--- CommandObject/SpamStringManager.cs
+++ CommandObject/SpamStringManager.cs
@@ -482,20 +482,11 @@ namespace TWBlackListSoamChecker.CommandObject
 
             Dictionary<string, string> banValues =
                 CommandDecoder.cutKeyIsValue(RawMessage.text.Substring(spacePath + 1));
-            string text = banValues.GetValueOrDefault("text", null);
             string rule = banValues.GetValueOrDefault("rule", null);
-            if (text == null)
-            {
-                TgApi.getDefaultApiConnection().sendMessage(
-                    RawMessage.GetMessageChatInfo().id,
-                    "你的輸入有錯誤",
-                    RawMessage.message_id
-                );
-                return;
-            }
 
             if (rule == null)
             {
+                string text = RawMessage.text.Replace("/point ","");
                 List<SpamMessage> spamMsgList = Temp.GetDatabaseManager().GetSpamMessageList();
                 string msg = "";
                 bool found = false;
@@ -552,6 +543,7 @@ namespace TWBlackListSoamChecker.CommandObject
             }
             else
             {
+                string text = RawMessage.text.Replace("/point ","").Replace("rule=","").Replace(rule,"");
                 SpamMessage smsg = Temp.GetDatabaseManager().GetSpamRule(rule);
                 if (smsg == null)
                 {
