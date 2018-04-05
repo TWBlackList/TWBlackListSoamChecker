bac654b2d627db6c150ec6206740eb9d7423cb9a
OwO <OwO@OmO.tw>
2018-04-05 22:29:53 +0800

    [AutoLeave] Fix ParseMode

--- SpamMessageDeleter.cs
+++ SpamMessageDeleter.cs
@@ -39,8 +39,7 @@ namespace TWBlackListSoamChecker
                 new Thread(delegate()
                 {
                     TgApi.getDefaultApiConnection().sendMessage(BaseMessage.GetMessageChatInfo().id, 
-                        "群管理必須加入[項目群組](https://t.me/" + Temp.ReportGroupName + ")才可使用本服務。",
-                        TgApi.PARSEMODE_MARKDOWN);
+                        "群管理必須加入[項目群組](https://t.me/" + Temp.ReportGroupName + ")才可使用本服務。",ParseMode: TgApi.PARSEMODE_MARKDOWN);
                     Thread.Sleep(2000);
                     TgApi.getDefaultApiConnection().leaveChat(BaseMessage.GetMessageChatInfo().id);
                 }).Start();
