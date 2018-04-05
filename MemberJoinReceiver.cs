5716476a3fc1cedef225f385acc44e8e1fb1e44e
OwO <OwO@OmO.tw>
2018-04-05 22:15:17 +0800

    [autoLeave] fix var

--- MemberJoinReceiver.cs
+++ MemberJoinReceiver.cs
@@ -78,7 +78,7 @@ namespace TWBlackListSoamChecker
                 {
                     new Thread(delegate()
                     {
-                        TgApi.getDefaultApiConnection().sendMessage(BaseMessage.GetMessageChatInfo().id, 
+                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id, 
                             "群管理必須加入[項目群組](https://t.me/" + Temp.ReportGroupName + ")才可使用本服務。",
                             TgApi.PARSEMODE_MARKDOWN);
                         Thread.Sleep(2000);
