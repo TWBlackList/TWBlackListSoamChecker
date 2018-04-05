c8c4938dfab542896a8392ab744d42d2a32df857
OwO <OwO@OmO.tw>
2018-04-05 22:12:53 +0800

    [autoLeave] fix msg

--- MemberJoinReceiver.cs
+++ MemberJoinReceiver.cs
@@ -78,7 +78,9 @@ namespace TWBlackListSoamChecker
                 {
                     new Thread(delegate()
                     {
-                        TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id, "群管理必須加入本項目回報群組才可使用本服務。");
+                        TgApi.getDefaultApiConnection().sendMessage(BaseMessage.GetMessageChatInfo().id, 
+                            "群管理必須加入[項目群組](https://t.me/" + Temp.ReportGroupName + ")才可使用本服務。",
+                            TgApi.PARSEMODE_MARKDOWN);
                         Thread.Sleep(2000);
                         TgApi.getDefaultApiConnection().leaveChat(RawMessage.GetMessageChatInfo().id);
                     }).Start();
